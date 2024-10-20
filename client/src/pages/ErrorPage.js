659d4448984adb4ba77008f6f7dcfbdd2befb5ad
Matt-Verni <matthewpmendez@gmail.com>
2024-10-20 17:27:00 -0400

    Start up code for pages and tsconfig modified

---
+++ client/src/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+    return (
+      <section>
+        <h1>404: Page Not Found</h1>
+        <h1>(°ロ°)☝</h1>
+      </section>
+    );
+  };
+  
+  export default ErrorPage;
+  
